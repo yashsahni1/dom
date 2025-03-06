415a372a23b24332640110bf92fa4ad11566ca42
yash sahni <sahniyash96@gmail.com>
2025-03-06 18:29:23 +0530

    yes i did

---
+++ script.js
@@ -0,0 +1,11 @@
+
+let myDiv = document.querySelector("#myDiv");
+
+// Applying styles
+myDiv.style.backgroundColor = "yellow";
+myDiv.style.margin = "20px";
+myDiv.style.padding = "10px";
+myDiv.style.fontSize = "18px";
+myDiv.style.fontWeight = "bold";
+myDiv.style.height = "200px";
+myDiv.style.width = "300px";
